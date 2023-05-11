72e998387f438ab16ff25fa69fc4fea29799e4ae
Joe Losee <josephlosee@gmail.com>
2023-05-11 08:33:45 -0700

    Removed initial setup, will make CSS changes later.

--- src/app.tsx
+++ src/app.tsx
@@ -8,21 +8,13 @@ export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
+      <h1>Fullstack App Built with Vite + Preact</h1>
+      <div className ="page-body" class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
+          A simple blog app based on a LinkedInLearning Course, but in Preact and Vite
         </p>
       </div>
       <p class="read-the-docs">
