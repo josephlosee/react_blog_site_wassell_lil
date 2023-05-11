7ece31cc013ffc6a554c7d7eaddf3e1f05b27877
Joe Losee <josephlosee@gmail.com>
2023-05-11 09:00:07 -0700

    Ch01-04 Pages and Router Setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './app.css'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticleListPage from './pages/ArticlesListPage'
+import ArticlePage from './pages/ArticlePage'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <h1>Fullstack App Built with Vite + Preact</h1>
       <div className ="page-body" class="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -16,10 +20,16 @@ export function App() {
         <p>
           A simple blog app based on a LinkedInLearning Course, but in Preact and Vite
         </p>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/articles" element={<ArticleListPage/>}/>
+          <Route path="/articles/:articleId" element={<ArticlePage/>}/>
+        </Routes>
       </div>
-      <p class="read-the-docs">
+      {/* <p class="read-the-docs">
         Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+      </p> */}
+    </BrowserRouter>
   )
 }
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+const NotFoundPage  = () => {
+    return (
+        <h1>Requested page not found!</h1>
+    );
+}
+
+export default NotFoundPage;
