9a6c25d404731f9a2336d251e438abb77a988c2e
Joseph Losee <josephlosee@gmail.com>
2023-05-24 14:37:14 -0700

    Chapter4 - Integration (#2)
    * Ch4.4 - Completed - Added axios and basic data fetching with useEffect
    
    Article pages now fetch article information
    
    Other:
    Added proxy configuration to vite.
    
    * Ch4.5 - Completed - Added comments display to Articles
    
    * Ch4.6 - Completed adding Upvote button and functionality to make put request
    
    * Chapter 4 Completed - Added Comment Form
    
    Article pages now have a basic Add Comment Form
    **No error handling**
    
    Other
    Cleaned up a couple stray semicolons in JSX
    Additional typing
    Minor difference: passed the setArticleInfo function directly as a prop rather than a new function.

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,24 +1,57 @@
+import { useState, useEffect } from "preact/hooks";
 import { useParams } from "react-router";
+import axios from "axios";
 import articles from "./ArticleContent";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList, {Comment} from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
+
+interface ArticleInfo {
+    upvotes: number,
+    comments: Comment[]
+}
 
 const ArticlePage  = () => {
     const params = useParams();
-
+    const [articleInfo, setArticleInfo] = useState<ArticleInfo>({upvotes: 0, comments: [] as Comment[]});
+    
     const articleId = params.articleId;
-    const article = articles.find(article => article.name === articleId);
     // can also be const {articleId} = useParams();
 
-    if (article === undefined){
+    const addUpvote = async() => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        setArticleInfo(response.data);
+    }
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data as ArticleInfo;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, [articleId]);
+
+    // console.log(articleInfo.comments);
+    const article = articles.find(article => article.name === articleId);
+    
+
+    if (!article){
         return <NotFoundPage/>;
     }
 
     return (
         <>
             <h1>{article.title}</h1>
+            <div className="upvotes-section">
+                <button onClick={addUpvote}>Upvote</button>
+                <p>This article has {articleInfo.upvotes} upvote(s).</p>
+            </div>
             {article.content.map((paragraph, index) => (
                 <p key={`${article.name}-${index}`}>{paragraph}</p>
             ))}
+            <AddCommentForm articleName={articleId!} onArticleUpdated={setArticleInfo}/>
+            <CommentsList comments={articleInfo.comments}/>
         </>
         
     );
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,26 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,      
+        ws: false,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+    }
+  }
+  }
 })
