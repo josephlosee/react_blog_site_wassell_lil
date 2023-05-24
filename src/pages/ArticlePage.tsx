c9523fdf5f583064f5923b2ef30a6898994e329f
Joe Losee <josephlosee@gmail.com>
2023-05-23 18:16:16 -0700

    Ch4.6 - Completed adding Upvote button and functionality to make put request

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -17,6 +17,11 @@ const ArticlePage  = () => {
     const articleId = params.articleId;
     // can also be const {articleId} = useParams();
 
+    const addUpvote = async() => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        setArticleInfo(response.data);
+    }
+
     useEffect(() => {
         const loadArticleInfo = async () => {
             const response = await axios.get(`/api/articles/${articleId}`);
@@ -37,7 +42,10 @@ const ArticlePage  = () => {
     return (
         <>
             <h1>{article.title}</h1>
-            <p>This article has {articleInfo.upvotes} upvote(s).</p>
+            <div className="upvotes-section">
+                <button onClick={addUpvote}>Upvote</button>
+                <p>This article has {articleInfo.upvotes} upvote(s).</p>
+            </div>
             {article.content.map((paragraph, index) => (
                 <p key={`${article.name}-${index}`}>{paragraph}</p>
             ))}
