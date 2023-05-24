644182b00beca921b6fe74d58753b8875e737a93
Joe Losee <josephlosee@gmail.com>
2023-05-23 17:47:46 -0700

    Ch4.4 - Completed - Added axios and basic data fetching with useEffect
    Article pages now fetch article information
    
    Other:
    Added proxy configuration to vite.

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "preact/hooks";
 import { useParams } from "react-router";
+import axios from "axios";
 import articles from "./ArticleContent";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage  = () => {
     const params = useParams();
-
+    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
+    
     const articleId = params.articleId;
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, [articleId]);
+
     const article = articles.find(article => article.name === articleId);
     // can also be const {articleId} = useParams();
 
@@ -16,6 +29,7 @@ const ArticlePage  = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s).</p>
             {article.content.map((paragraph, index) => (
                 <p key={`${article.name}-${index}`}>{paragraph}</p>
             ))}
