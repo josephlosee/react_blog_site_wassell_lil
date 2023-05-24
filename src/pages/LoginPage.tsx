3e834b500c39fde1fd47f597ecdade19f623fa45
Joe Losee <josephlosee@gmail.com>
2023-05-24 16:27:50 -0700

    Ch 5.4 - Completed Login Form
    Basic Login Form for sign in with email using firebase

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,51 @@
 // interface LoginPageProps {
 
+import { useState } from "preact/hooks";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 // }
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const logIn = async () => {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        } catch (e) {
+            setError(e.message);
+        }
+
+    }
+
     return (
         <>
             <h1>Login</h1>
+            {error && <p className="error">{error}</p>}
+            <label>
+                Email
+                <input 
+                    type="text" 
+                    value={email}
+                    onChange={e => setEmail((e!.target as HTMLInputElement).value)}></input>
+            </label>
+            <br/>
+            <label>
+                Password
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword((e!.target as HTMLInputElement).value)}>
+                </input>
+            </label>
+            <br/>
+            <button onClick={logIn}>Log In</button>
+            <Link to="/create-account">Don't have an account? Create one here!</Link>
+            {/* <button onClick={createAccount}>Create</button> */}
         </>
     );
 }
